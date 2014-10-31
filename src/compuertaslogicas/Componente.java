389fcc9a5e1745a7ad211c5bc66e942a255d7bda
jairo <jairo@jairo-TOSHIBA-NB505>
2014-10-30 22:59:48 -0600

    Objeto Componente mejorado

--- src/compuertaslogicas/Componente.java
+++ src/compuertaslogicas/Componente.java
@@ -13,27 +13,24 @@ package compuertaslogicas;
 public class Componente {
     private String _tipo;
     private String _imagen;
-    private String _sigtConexion;
+    private Componente _sigtConexion;
     private int _numEntradas;
     private int _numEntradasRestantes;
     private Lista _valorEntrada;
-    private int _cero=0;
-    private int _uno=1;
     private double _posx;
     private double _posy;
         
     
-    public Componente (String _tipo, String _imagen, int _numEntradas, String _sigtConexion, Lista _valorEntrada, double _posx, double _posy){
+    public Componente (String _tipo, String _imagen, int _numEntradas, Lista _valorEntrada, double _posx, double _posy){
         this._tipo = _tipo;
         this._imagen = _imagen;
         this._numEntradas = _numEntradas;
-        this._sigtConexion = _sigtConexion;
         this._valorEntrada=_valorEntrada;
         this._numEntradasRestantes=0;
         this._posx=_posx;
         this._posy=_posy;
-    }
-        
+        this._sigtConexion = null;
+    }   
     
     public String getTipo(){
         return this._tipo;
@@ -43,20 +40,15 @@ public class Componente {
         return this._imagen;
     }
     
-    public int getEntradas(){
+    public int getNumEntradas(){
         return this._numEntradas;
     }
     
-    public void setEntradas(int entradas){
-        this._numEntradas=entradas;
-        
-    }
-    
-    public String getsigtConexion(){
+    public Componente getSigtConexion(){
         return this._sigtConexion;
     }
     
-    public void setOut (String _sigtConexion){
+    public void setSigtConexion (Componente _sigtConexion){
         this._sigtConexion = _sigtConexion;        
     }
     
@@ -64,14 +56,8 @@ public class Componente {
         return this._numEntradasRestantes;
     }
     
-    public void setEntradasRestantes(int entrada){
-        this._numEntradasRestantes+=entrada;
-    }
-    public int getValorEntrada(){
-        if (this._valorEntrada.getLength()==1)
-            return (int)this._valorEntrada.getHead().get_data();
-        else
-            return this._valorEntrada.getLength();
+    public Lista getValorEntrada(){
+        return this._valorEntrada;
         
     }
     public double getPosx(){
@@ -81,5 +67,12 @@ public class Componente {
     public double getPosy(){
         return this._posy;
     }
-    
+
+    public void setPosx(double _posx) {
+        this._posx = _posx;
+    }
+
+    public void setPosy(double _posy) {
+        this._posy = _posy;
+    }    
 }           
